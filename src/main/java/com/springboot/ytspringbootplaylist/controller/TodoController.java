20d311c1d9962c38862477168e910802decd3bcf
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-04-29 18:24:45 +0530

    CREATE : get specific todo implemented

--- src/main/java/com/springboot/ytspringbootplaylist/controller/TodoController.java
+++ src/main/java/com/springboot/ytspringbootplaylist/controller/TodoController.java
@@ -5,13 +5,11 @@ import com.springboot.ytspringbootplaylist.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TodoController {
@@ -38,5 +36,16 @@ public class TodoController {
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("/todos/{id}")
+    public ResponseEntity<?> getTodo(@PathVariable("id") String id) {
+        Optional<Todo> todoOptional = todoRepository.findById(id);
+        if(todoOptional.isPresent()) {
+            return new ResponseEntity<Optional>(todoOptional, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<String>("Todo not found with id " + id,
+                HttpStatus.NOT_FOUND);
     }
 }
